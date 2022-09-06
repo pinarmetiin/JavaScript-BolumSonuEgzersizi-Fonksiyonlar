631ca0581d0f48f3c989a0bf7e8556f519f37a4e
pinarmetiin <108363269+pinarmetiin@users.noreply.github.com>
2022-09-06 15:07:48 +0300

    Add files via upload

---
+++ JavaScript-BolumSonuEgzersizi-Fonksiyonlar/fonksiyonlar.js
@@ -0,0 +1,24 @@
+// degiskenler tanimlandi, ilgili id bilgilerine erisildi
+let counter = 0;
+let counterDOM = document.querySelector('#counter')
+let increaseDOM = document.querySelector('#increase')
+let decreaseDOM = document.querySelector('#decrease')
+
+// counterDom a baslangic degeri verildi
+counterDOM.innerHTML = counter
+
+// butonların click ozelligine erisildi.
+increaseDOM.addEventListener("click", clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+
+/* v1
+function clickEvent(){
+    this.id == "increase" ? counterDOM.innerHTML = counter +=1 :  counterDOM.innerHTML = counter -=1    
+} 
+*/ 
+
+// v2
+function clickEvent(){
+    this.id == "increase" ? counter +=1 : counter -=1 
+    counterDOM.innerHTML = counter   
+}
